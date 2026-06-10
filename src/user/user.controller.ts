ad5ba6d6edf88ca8f65c7adc21a0fce926824594
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-10 14:41:55 -0300

    feat: implement UserController with user creation and retrieval endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  GetAll(@Query('page') page = 1, @Query('limit') limit = 10) {
+    return this.userService.GetAll(page, limit);
+  }
+
+  @Post()
+  Create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+}
