8e635442b68beb257bfc5665ecb70cf812141352
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-15 16:00:46 -0300

    feat: add ResponseProductDto for product response handling

---
+++ src/product/dto/response-product.dto.ts
@@ -0,0 +1,25 @@
+import { Product } from 'src/product/entities/product.entity';
+
+export class ResponseProductDto {
+  id: number;
+  name: string;
+  purchasePrice: number;
+  salePrice: number;
+  quantity: number;
+  codigoQr: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: number;
+
+  constructor(user: Product) {
+    this.id = user.id;
+    this.name = user.name;
+    this.purchasePrice = user.purchasePrice;
+    this.salePrice = user.salePrice;
+    this.quantity = user.quantity;
+    this.codigoQr = user.codigoQr;
+    this.createdAt = user.createdAt;
+    this.updatedAt = user.updatedAt;
+    this.userId = user.userId;
+  }
+}
