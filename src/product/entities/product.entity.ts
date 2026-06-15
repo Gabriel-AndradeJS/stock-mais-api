99001bc6e017bb98d4cd349b9aec1fac6fa15440
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-15 16:00:51 -0300

    feat: create Product entity with properties and relationships

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,44 @@
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  purchasePrice: number;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  salePrice: number;
+
+  @Column()
+  quantity: number;
+
+  @Column('')
+  codigoQr: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+}
