cba22978ffe3bb50cb8a1b70b0b8656c9aaab3f0
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-16 11:15:15 -0300

    feat: add getProducts and updateProduct methods to ProductController

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { CreateProductDto } from 'src/product/dto/create-product.dto';
 import { ProductService } from 'src/product/product.service';
-
 @UseGuards(AuthGuard)
 @Controller('product')
 export class ProductController {
@@ -14,5 +23,22 @@ export class ProductController {
     @Body() createProductDto: CreateProductDto,
   ) {
     return this.productService.createProduct(req, createProductDto);
+  }
+
+  @Get()
+  getProducts(
+    @Req() req: Request,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ) {
+    return this.productService.getProducts(req, page, limit);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: number,
+    @Body() updateProductDto: CreateProductDto,
+  ) {
+    return this.productService.updateProduct(id, updateProductDto);
   }
 }
