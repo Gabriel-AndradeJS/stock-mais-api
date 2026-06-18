2dbd0a177e03c5e4e97b7ab4828d5ad0c237c72f
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-18 14:25:34 -0300

    feat: add TypeOrmModule and StockMovements entity to StockMovementsModule

--- src/stock-movements/stock-movements.module.ts
+++ src/stock-movements/stock-movements.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StockMovementsController } from './stock-movements.controller';
 import { StockMovementsService } from './stock-movements.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StockMovements } from 'src/stock-movements/entities/stock-movements.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StockMovements])],
   controllers: [StockMovementsController],
-  providers: [StockMovementsService]
+  providers: [StockMovementsService],
+  exports: [StockMovementsService],
 })
 export class StockMovementsModule {}
