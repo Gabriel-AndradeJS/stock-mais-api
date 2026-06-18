70ddf4c40151e97ee05ca9d2a17ed964639ea183
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-18 14:25:47 -0300

    feat: implement StockMovementsService with methods for retrieving and logging product movements

--- src/stock-movements/stock-movements.service.ts
+++ src/stock-movements/stock-movements.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StockMovements } from 'src/stock-movements/entities/stock-movements.entity';
+import { Repository } from 'typeorm';
+import { PartMovement } from 'src/common/enums/part-movements';
 
 @Injectable()
-export class StockMovementsService {}
+export class StockMovementsService {
+  constructor(
+    @InjectRepository(StockMovements)
+    private readonly stockMovementsRepository: Repository<StockMovements>,
+  ) {}
+
+  async getAllMovements() {
+    return await this.stockMovementsRepository.find({
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  async logProductMovement(
+    productId: number,
+    productName: string,
+    mechanicName: string,
+    mechanicalReviewId: number,
+    quantity: number,
+    movementType: PartMovement,
+  ) {
+    const movement = this.stockMovementsRepository.create({
+      productId,
+      productName,
+      mechanicName,
+      mechanicalReviewId,
+      quantity,
+      movementType,
+    });
+
+    return await this.stockMovementsRepository.save(movement);
+  }
+}
