685efb56367c2ebabaa748611d9e82ce0dd72ecb
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-19 10:53:38 -0300

    feat: add SalesController to handle sale creation

---
+++ src/sales/sales.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { CreateSaleDto } from './dto/create-sale.dto';
+import { SalesService } from './sales.service';
+
+@UseGuards(AuthGuard)
+@Controller('sales')
+export class SalesController {
+  constructor(private readonly salesService: SalesService) {}
+
+  @Post()
+  createSale(@Body() createSaleDto: CreateSaleDto) {
+    return this.salesService.createSale(createSaleDto);
+  }
+}
