4a6320abd5e92b8705b935af7d1c163d7f8b606b
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-19 15:08:37 -0300

    feat: implement BudgetController with methods for budget retrieval and creation

--- src/budget/budget.controller.ts
+++ src/budget/budget.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BudgetService } from 'src/budget/budget.service';
+import { CreateBudgetDto } from 'src/budget/dto/create-budget.dto';
 
 @Controller('budget')
-export class BudgetController {}
+export class BudgetController {
+  constructor(private readonly budgetService: BudgetService) {}
+
+  @Get()
+  findAllBudgets() {
+    return this.budgetService.findAllBudgets();
+  }
+
+  @Post()
+  createBudget(@Body() createBudgetDto: CreateBudgetDto) {
+    return this.budgetService.createBudget(createBudgetDto);
+  }
+}
