109b17a9863d5c16a4db5abd37b631be588ceab5
Gabriel Andrade <gabriel.andrade@azultecnologia.com>
2026-06-19 15:08:41 -0300

    feat: add TypeOrmModule and ProductModule imports to BudgetModule

--- src/budget/budget.module.ts
+++ src/budget/budget.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BudgetController } from './budget.controller';
 import { BudgetService } from './budget.service';
+import { ProductModule } from 'src/product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Budget } from 'src/budget/entities/budget.entity';
 
 @Module({
+  imports: [ProductModule, TypeOrmModule.forFeature([Budget])],
   controllers: [BudgetController],
-  providers: [BudgetService]
+  providers: [BudgetService],
 })
 export class BudgetModule {}
